4c6006e2e62f287f0bdf110c559819b07c8ab074
ElTinmar <martin.privat@hotmail.fr>
2024-03-23 05:43:42 +0100

    move import

--- camera_tools/tests/test_camera_widgets.py
+++ camera_tools/tests/test_camera_widgets.py
@@ -1,17 +1,18 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from camera_tools import (
-    CameraPreview, RandomCam, XimeaCamera, CameraControl
+    CameraPreview, RandomCam, CameraControl
 )
 import numpy as np
 
 if __name__ == '__main__':
 
-    USE_XIMEA = True
+    USE_XIMEA = False
 
     app = QApplication(sys.argv)        
     
     if USE_XIMEA:
+        from camera_tools import XimeaCamera
         cam = XimeaCamera(1)
     else:
         cam = RandomCam(shape=(512,512), dtype=np.uint8)
