24930c72687fca58c8fb0e14d8451021299c07b9
ElTinmar <martin.privat@hotmail.fr>
2024-03-23 06:51:49 +0100

    use different cameras

--- camera_tools/tests/test_camera_widgets.py
+++ camera_tools/tests/test_camera_widgets.py
@@ -1,24 +1,33 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from camera_tools import (
-    CameraPreview, RandomCam, CameraControl
+    CameraPreview, CameraControl
 )
 import numpy as np
 
 if __name__ == '__main__':
 
     USE_XIMEA = False
+    USE_RANDOM = False
+    USE_WEBCAM = True
 
     app = QApplication(sys.argv)        
     
     if USE_XIMEA:
         from camera_tools import XimeaCamera
         cam = XimeaCamera(1)
-    else:
+
+    if USE_RANDOM:
+        from camera_tools import RandomCam
         cam = RandomCam(shape=(512,512), dtype=np.uint8)
 
+    if USE_WEBCAM:
+        from camera_tools import OpenCV_Webcam
+        cam = OpenCV_Webcam()
+        
     controls = CameraControl(cam)
     window = CameraPreview(controls)
     window.show()
+    
     sys.exit(app.exec())
 
