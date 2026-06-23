3b90715578d05ceec87e55de5c8a3a3937177b01
ElTinmar <martin.privat@hotmail.fr>
2026-06-23 11:53:46 +0200

    bump version  cam_tools

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='camera_tools',
     python_requires='>=3.8',
     author='Martin Privat',
-    version='0.8.11',
+    version='0.8.12',
     packages=['camera_tools'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='camera tools',
@@ -14,7 +14,7 @@ setup(
         "qtpy",
         "opencv-python-headless",
         "v4l2py",
-        "video_tools @ git+https://github.com/ElTinmar/video_tools.git@v0.6.8",
+        "video_tools @ git+https://github.com/ElTinmar/video_tools.git@v0.6.9",
         "qt_widgets @ git+https://github.com/ElTinmar/qt_widgets.git@v0.5.1"
     ]
 )
